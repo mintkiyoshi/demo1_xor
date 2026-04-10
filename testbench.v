18926cfaca4ed8a2ab56d25d2d0bbb11141d6128
Byron Mojica <byronlacanlale1@gmail.com>
2026-04-10 23:22:02 +0800

    Initial commit

---
+++ testbench.v
@@ -0,0 +1,26 @@
+// testbench for xor module
+
+module testbench;
+	//tb signals
+	reg a;
+	reg b;
+	wire y;
+	
+	// instantiate xor module
+	xor_gate dut(
+		.Y(y),
+		.A(a),
+		.B(b)
+	);
+		
+	
+	//apply stimuli
+	initial begin 
+		a=0;b=0; #10;
+		a=0;b=1; #10;
+		a=1;b=0; #10;
+		a=1;b=1; #10;
+	end
+	
+	
+endmodule
