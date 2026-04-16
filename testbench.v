b6365f478f9c3312d461dac9cb6156cc6cdbafbb
Byron Mojica <byronlacanlale1@gmail.com>
2026-04-16 20:34:06 +0800

    dataflow of demo1_xor

--- testbench.v
+++ testbench.v
@@ -2,24 +2,24 @@
 
 module testbench;
 	//tb signals
-	reg a;
-	reg b;
-	wire y;
+	reg A;
+	reg B;
+	wire Y;
 	
 	// instantiate xor module
 	xor_gate dut(
-		.Y(y),
-		.A(a),
-		.B(b)
+		.Y(Y),
+		.A(A),
+		.B(B)
 	);
 		
 	
 	//apply stimuli
 	initial begin 
-		a=0;b=0; #10;
-		a=0;b=1; #10;
-		a=1;b=0; #10;
-		a=1;b=1; #10;
+		A=0;B=0; #10;
+		A=0;B=1; #10;
+		A=1;B=0; #10;
+		A=1;B=1; #10;
 	end
 	
 	
